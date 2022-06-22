36f83517db29d9c3719b51fe8c71751d34a2865d
shenjiahao <1843781563@qq.com>
2022-06-22 19:47:03 +0800

    更新model/base.go

--- chainmaker-contract-sdk-docker-go/model/base.go
+++ chainmaker-contract-sdk-docker-go/model/base.go
@@ -1,6 +1,9 @@
 package model
 
-const FileKey = "file-key"
+const (
+	FileKey = "file-key"
+	LogKey  = "log-key"
+)
 
 // 生码记录
 type GenerateQRLog struct {
@@ -25,5 +28,28 @@ type VerifyQRLog struct {
 
 // 用户授权记录
 type UserAuthorizeLog struct {
-	AuthorizerID string `json:"authorizer"`// shou
+	AuthorizerID     string `json:"authorizerID"`     // 授权人身份标识
+	AuthorizeeID     string `json:"authorizeeID"`     // 被授权人身份标识
+	AuthorizeTime    string `json:"authorizeTime"`    // 授权时间
+	AuthorizeContent string `json:"authorizeContent"` // 授权内容
+	GeneratorID      string `json:"generatorID"`      // 生码方身份标识
+}
+
+type VerifyUserIdentityLog struct {
+	NameHash         string `json:"nameHash"`         // 姓名哈希
+	IdentityCardHash string `json:"identityCardHash"` // 身份证号哈希
+	TelephoneHash    string `json:"telephoneHash"`    // 手机号Hash
+	VerifyStatus     string `json:"verifyStatus"`     // 核验状态
+}
+
+type AssociateIdentityLog struct {
+	IdentityCardHash string `json:"identityCardHash"` // 身份证号哈希
+	UserID           string `json:"userID"`           // 用户数字身份
+	PID              string `json:"pid"`              // 平台标识符
+}
+
+type CancelAuthorizeLog struct {
+	AuthorizerID    string `json:"authorizerID"`    // 授权人身份标识
+	AuthorizeeID    string `json:"authorizeeID"`    // 被授权人身份标识
+	CanceledContent string `json:"canceledContent"` // 取消授权内容
 }
