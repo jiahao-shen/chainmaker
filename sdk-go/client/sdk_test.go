880088d4982ac616a309c4f3a8a692960e3720d0
shenjiahao <1843781563@qq.com>
2022-08-10 17:18:02 +0800

    更新sdk-go/client/sdk_test.go

--- sdk-go/client/sdk_test.go
+++ sdk-go/client/sdk_test.go
@@ -40,17 +40,57 @@ func TestInstallContract(t *testing.T) {
 
 }
 
-func TestQueryContr(t *testing.T) {
+func TestQueryContract(t *testing.T) {
 	client, err := sdk.NewChainClient(sdk.WithConfPath("sdk_config.yaml"))
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	resp, err := client.QueryContract("test", "hello", nil, 5)
+	params := []*common.KeyValuePair{
+		{
+			Key:   "method",
+			Value: []byte("hello"),
+		},
+	}
+
+	resp, err := client.QueryContract("test", "invoke_contract", params, -1)
 	if err != nil {
-		fmt.Println("fuck")
 		fmt.Println(err)
 	}
 
 	fmt.Println(resp)
+}
+
+func TestInvokeContract(t *testing.T) {
+	client, err := sdk.NewChainClient(sdk.WithConfPath("sdk_config.yaml"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	params := []*common.KeyValuePair{
+		{
+			Key:   "method",
+			Value: []byte("saveFile"),
+		},
+		{
+			Key:   "file_name",
+			Value: []byte("test"),
+		},
+		{
+			Key:   "file_hash",
+			Value: []byte("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"),
+		},
+		{
+			Key:   "time",
+			Value: []byte("2022-08-10 17:15:55"),
+		},
+	}
+
+	resp, err := client.InvokeContract("test", "invoke_contract", "", params, -1, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(resp)
+
 }
